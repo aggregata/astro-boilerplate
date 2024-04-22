3fa01b830f82da517d22c4f39391beaa35b2b93c
Jairus Joer <jairus.joer@hero-software.de>
2024-04-22 18:59:02 +0200

    Add Astro sitemap integration

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from "astro/config";
+
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   srcDir: ".",
+  site: "http://localhost:4321/",
   integrations: [
     mdx(),
+    sitemap(),
     tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+      applyBaseStyles: false
+    })
+  ]
 });
